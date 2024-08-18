4163f2e69934b7a83445ba5474a2cb08850531cc
Aweroro Hadi <hadistic20@gmail.com>
2024-08-18 03:26:53 +0100

    Setting up of routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import React,{useState} from 'react';
+import Header from './components/Header';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
   return (
-    <div>Hi</div>
+    <div className={`min-h-screen ${darkMode? 'dark bg-gray-900 text-white' : 'bg-white text-gray-900'}`}>
+      <Header darkMode={darkMode} setDarkMode={setDarkMode}/>
+    </div>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = ({darkMode, setDarkMode}) => {
+  return (
+    <div>
+      <header className='bg-gray-100 dark:bg-gray-800 py-4'>
+        <div></div>
+      </header>
+    </div>
+  )
+}
+
+export default Header;
