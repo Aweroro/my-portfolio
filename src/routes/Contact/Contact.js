07f8450fc75e4e8d75dfa37a51d8be4ec15444aa
Aweroro Hadi <hadistic20@gmail.com>
2024-08-31 16:18:29 +0100

    adjusted contact icons

--- src/routes/Contact/Contact.js
+++ src/routes/Contact/Contact.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import LocationIcon from '../../assets/location-icon';
-import PhoneIcon from '../../assets/phone-icon';
-import MailIcon from '../../assets/mail-icon'
+//import LocationIcon from '../../assets/location-icon';
+//import PhoneIcon from '../../assets/phone-icon';
+//import MailIcon from '../../assets/mail-icon'
 import { FaTwitter, FaGithub, FaInstagram } from 'react-icons/fa';
+import { PhoneIcon, EnvelopeIcon, MapIcon } from '@heroicons/react/16/solid';
 
 const Contact = () => {
   return (
@@ -11,21 +12,26 @@ const Contact = () => {
       <div className="flex flex-col md:flex-row mx-24 gap-x-48">
         <div className='flex flex-col'>
         <div className="flex-1 mb-4">
-          {/* <h3 className="text-xl font-semibold mb-4">Get in touch</h3> */}
           <ul className="space-y-12">
             <li className='flex space-x-2'>
-              <MailIcon/>
+              <div className='flex space-x-2'>
+              <EnvelopeIcon className='size-12 rounded-lg px-2 bg-slate-900'/>
               <a href="mailto:hadistic20@gmail.com" target="_blank" rel="noopener noreferrer" className="text-white hover:underline">
                 hadistic20@gmail.com
               </a>
+              </div>
             </li>
             <li className='flex space-x-2'>
-              <PhoneIcon/>
+              <div className='flex space-x-2'>
+              <PhoneIcon className='size-12 rounded-lg px-2 bg-slate-900'/>
               <p>08082742862</p>
+              </div>
             </li>
             <li className='flex space-x-2'>
-              <LocationIcon/>
+              <div className='flex space-x-2'>
+              <MapIcon className='size-12 rounded-lg px-2 bg-slate-900'/>
               <p>Lagos, Nigeria</p>
+              </div>
             </li>
           </ul>
         </div>
