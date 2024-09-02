579331fe9f91d8b076b14eff70bc59de5133086f
Aweroro Hadi <hadistic20@gmail.com>
2024-09-02 20:17:32 +0100

    changes

--- src/routes/Contact/Contact.js
+++ src/routes/Contact/Contact.js
@@ -4,25 +4,24 @@ import { PhoneIcon, EnvelopeIcon, MapPinIcon } from '@heroicons/react/24/solid';
 
 const Contact = () => {
   return (
-    <div className='py-10 text-white'>
-      <h2 className="text-lg font-bold ml-6 md:ml-24 mb-6 uppercase">Contact Info</h2> 
-      <div className="flex flex-col md:flex-row mx-6 md:mx-24 gap-10 md:gap-x-48"> 
-        
+    <div className='py-10 text-white'> 
+      <div className="flex flex-col-reverse md:flex-row mx-6 md:mx-24 gap-10 md:gap-x-48"> 
         <div className='flex flex-col'>
           <div className="flex-1 mb-8">
+          <h2 className="text-lg font-bold ml-6 md:ml-24 mb-6 uppercase">Contact Info</h2>
             <ul className="space-y-6"> 
               <li className='flex space-x-4 items-center'>
-                <EnvelopeIcon className='w-8 h-8 p-1 md:w-12 md:h-12 md:p-2 rounded-lg bg-slate-800'/> {/* Adjusted icon size */}
+                <EnvelopeIcon className='w-8 h-8 p-1 md:w-12 md:h-12 md:p-2 rounded-lg bg-slate-800'/> 
                 <a href="mailto:hadistic20@gmail.com" target="_blank" rel="noopener noreferrer" className="text-white hover:underline">
                   hadistic20@gmail.com
                 </a>
               </li>
               <li className='flex space-x-4 items-center'>
-                <PhoneIcon className='w-8 h-8 p-1 md:w-12 md:h-12 md:p-2 rounded-lg bg-slate-800'/> {/* Adjusted icon size */}
+                <PhoneIcon className='w-8 h-8 p-1 md:w-12 md:h-12 md:p-2 rounded-lg bg-slate-800'/> 
                 <p>08082742862</p>
               </li>
               <li className='flex space-x-4 items-center'>
-                <MapPinIcon className='w-8 h-8 p-1 md:w-12 md:h-12 md:p-2 rounded-lg bg-slate-800'/> {/* Adjusted icon size */}
+                <MapPinIcon className='w-8 h-8 p-1 md:w-12 md:h-12 md:p-2 rounded-lg bg-slate-800'/> 
                 <p>Lagos, Nigeria</p>
               </li>
             </ul>
@@ -56,8 +55,8 @@ const Contact = () => {
         
         <div className="flex-1 border rounded-lg p-4 md:p-6">
           <div className='flex gap-2 items-baseline'>
-            <h3 className="text-4xl md:text-5xl font-semibold mb-4">Get in</h3> {/* Adjusted font size for smaller screens */}
-            <h3 className="text-4xl md:text-5xl text-[#2962ff] font-semibold mb-4">touch.</h3> {/* Adjusted font size for smaller screens */}
+            <h3 className="text-4xl md:text-5xl font-semibold mb-4">Get in</h3> 
+            <h3 className="text-4xl md:text-5xl text-[#2962ff] font-semibold mb-4">touch.</h3> 
           </div>
           <form className="space-y-4">
             <div className='text-white'>
